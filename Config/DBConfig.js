2717a7c2eacf7bea8936d5e3a9c1bf3aaaab79e5
JegankarthiMCA <140585367+JegankarthiMCA@users.noreply.github.com>
2024-03-15 21:57:43 +0530

    Update DBConfig.js

--- Config/DBConfig.js
+++ Config/DBConfig.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const DBconfig = async () => {
     try {
         // await mongoose.connect('mongodb://localhost/bank')
-        await mongoose.connect('mongodb+srv://jegankjack121:123@cluster0.qbb81tx.mongodb.net/?retryWrites=true&w=majority')
+        await mongoose.connect('mongodb+srv://jegankjack121:987123@cluster0.qbb81tx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         console.log("DB connected");
     } catch (error) {
         console.log("couldn't connect DB", error);
